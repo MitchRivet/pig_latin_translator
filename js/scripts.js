af50bab01fc2007983dd41c610a44c30db22c67c
Mitch Rivet & William Swanson <student@epicodus.com>
2015-09-09 15:28:57 -0700

    add ability to handle multiple consonants

--- js/scripts.js
+++ js/scripts.js
@@ -17,7 +17,13 @@ var pigLatin = function(string) {
       // console.log(word.charAt(0));
       var newWord = (word.concat("ay"));
     }
-    
+
+    else if (consonantArray.indexOf(letterArray[0]) != -1 && consonantArray.indexOf(letterArray[1]) != -1)
+    {
+      word += word.charAt(0) + word.charAt(1) + "ay";
+      var newWord = word.replace((word.substr(0,2)), "");
+    }
+
     else if (consonantArray.indexOf(word.charAt(0)) != -1)
     {
       // console.log(word.charAt(0));
